08d15ce25bf74cf2a86c985d0cc4108dd4a0d704
Mosarrof Hossain <phmosharaf@gmail.com>
2018-06-27 02:38:28 -0400

    My Smart cat are talking

--- src/catsound/Cat.java
+++ src/catsound/Cat.java
@@ -13,11 +13,12 @@ public class Cat{
 	public void run(int feet) {
 	    
 	    System.out.println(" >>>>>> \nHey!! I can ran " + feet + " Kmh!");
+	    System.out.println(" >>>>>> You don't care me right! ");
 
 		}
 public void color() {
 	    
-	    System.out.println(" >>>>>> \nHelloo!! Do you know..My color is White!");
+	    System.out.println(" >>>>>> \nHelloo!!! Do you know..My color is White!");
 
 		}
 	public static void main(String[] args) {
