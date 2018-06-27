181f82fd1c80fccabf1918a804b05cfc94d5de7d
Mosarrof Hossain <phmosharaf@gmail.com>
2018-06-27 02:10:31 -0400

    Cat Run coding changed

--- src/catsound/Cat.java
+++ src/catsound/Cat.java
@@ -1,21 +1,25 @@
 package catsound;
 
-public class Cat {
+public class Cat{
 	int catSound;
-	public Cat(int Age) {
+	public Cat(int Age){
 		Age = catSound;
 	}
-	public void CatCall() {
+	public void CatCall(){
 		for(int Age=1; Age<5;Age++)
-		System.out.println("Meu>>>>Meu-I would like to eat some fish.");
-		
+		System.out.println("Meu>>>>Meu-I would like to eat some fish.");	
 	}
-	public void Cat(){
-		
-	}
+	public void run(int feet) {
+	    
+	    System.out.println(" >>>>>> \nHey!! I can ran " + feet + " Kmh!");
+
+		}
 	public static void main(String[] args) {
-	Cat Sound = new Cat(18);
+		
+	Cat Sound = new Cat(8);
 	Sound.CatCall();
+	Sound.run(10);
+	
 	}
 
 }
