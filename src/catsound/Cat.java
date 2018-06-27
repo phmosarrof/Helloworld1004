45a0b97ca00206d2ed13604e377ea6561298366d
Mosarrof Hossain <phmosharaf@gmail.com>
2018-06-27 01:28:57 -0400

    A responsive Cat calling Project

---
+++ src/catsound/Cat.java
@@ -0,0 +1,24 @@
+package catsound;
+
+public class Cat {
+	int catSound;
+	public Cat(int Age) {
+		Age = catSound;
+	}
+	public void CatCall() {
+		for(int Age=1; Age<5;Age++)
+		System.out.println("Meu>>>>Meu-I would like to eat some fish.");
+		
+	}
+	public void Cat(){
+		
+	}
+	public static void main(String[] args) {
+	Cat Sound = new Cat(18);
+	Sound.CatCall();
+	}
+
+}
+
+
+   
